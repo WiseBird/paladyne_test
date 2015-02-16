0bc05843b6fca4ca3ef07c7a19f7ab65311e86d6
Sergey <sokolcjrjk@gmail.com>
2015-02-16 10:56:19 +0300

    Added controllers tests

---
+++ code/Paladyne.Angularjs.Web.Tests/Controllers/UserModulesControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FakeItEasy;
+
+using NUnit.Framework;
+
+using Paladyne.Angularjs.BL;
+using Paladyne.Angularjs.BL.Services;
+using Paladyne.Angularjs.DAL.Entities;
+using Paladyne.Angularjs.DAL.Tests;
+using Paladyne.Angularjs.Web.Controllers;
+using Paladyne.Angularjs.Web.Infrastructure;
+using Paladyne.Angularjs.Web.Models.UserModules;
+
+namespace Paladyne.Angularjs.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class UserModulesControllerTests
+    {
+        [Test]
+        public void Put_Always_CallsUserModuleServiceUpdate()
+        {
+            var userService = A.Fake<IUserService>();
+            A.CallTo(() => userService.GetByName(A<string>._)).Returns(new User());
+
+            var mockUserModuleService = A.Fake<IUserModuleService>();
+
+            var controller = CreateController(userService, mockUserModuleService);
+            
+
+            Helper.Suppress(() => controller.Put("", new UpdateUserModule()));
+
+
+            A.CallTo(() => mockUserModuleService.Update(A<BL.Models.UpdateUserModule>._, A<IValidationErrors>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public void Put_WhenWalidationError_ReturnsServiceErrorsResult()
+        {
+            var userService = A.Fake<IUserService>();
+            A.CallTo(() => userService.GetByName(A<string>._)).Returns(new User());
+
+            var userModuleService = A.Fake<IUserModuleService>();
+            A.CallTo(() => userModuleService.Update(A<BL.Models.UpdateUserModule>._, A<IValidationErrors>._)).Invokes(
+                    (BL.Models.UpdateUserModule model, IValidationErrors errors) => errors.Add("", "error"));
+
+            var controller = CreateController(userService, userModuleService);
+
+
+            var result = controller.Put("", new UpdateUserModule());
+
+
+            Assert.IsInstanceOf<ServiceErrorsResult>(result);
+        }
+
+        private static UserModulesController CreateController(IUserService userService = null, IUserModuleService userModuleService = null)
+        {
+            userService = userService ?? A.Fake<IUserService>();
+            userModuleService = userModuleService ?? A.Fake<IUserModuleService>();
+
+            return new UserModulesController() { UserService = userService, UserModuleService = userModuleService };
+        }
+    }
+}
---
+++ code/Paladyne.Angularjs.Web.Tests/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FakeItEasy;
+
+using NUnit.Framework;
+
+using Paladyne.Angularjs.BL;
+using Paladyne.Angularjs.BL.Models;
+using Paladyne.Angularjs.BL.Services;
+using Paladyne.Angularjs.DAL.Entities;
+using Paladyne.Angularjs.DAL.Tests;
+using Paladyne.Angularjs.Web.Controllers;
+using Paladyne.Angularjs.Web.Infrastructure;
+
+namespace Paladyne.Angularjs.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        [Test]
+        public void Put_Always_SetsModuleGrantrIdToCurrentUser()
+        {
+            const string currentUserId = "currentUserId";
+
+            var mockUserService = A.Fake<IUserService>();
+            A.CallTo(() => mockUserService.GetByName(A<string>._)).Returns(new User() { Id = currentUserId });
+
+            var controller = CreateController(mockUserService);
+
+            var model = new UpdateUserData();
+            model.Modules.Add(new UpdateUserData.UserModule() { GranterId = "fake" });
+
+
+            Helper.Suppress(() => controller.Put("", model));
+
+
+            A.CallTo(() => mockUserService.Update(A<UpdateUserData>._, A<IValidationErrors>._)).WhenArgumentsMatch(
+                (x) =>
+                    {
+                        var mdl = x.Get<UpdateUserData>(0);
+                        return mdl.Modules.Any(y => y.GranterId != currentUserId);
+                    });
+        }
+
+        [Test]
+        public void Put_Always_CallsUserServiceUpdate()
+        {
+            var mockUserService = A.Fake<IUserService>();
+            A.CallTo(() => mockUserService.GetByName(A<string>._)).Returns(new User());
+
+            var controller = CreateController(mockUserService);
+
+
+            Helper.Suppress(() => controller.Put("", new UpdateUserData()));
+
+
+            A.CallTo(() => mockUserService.Update(A<UpdateUserData>._, A<IValidationErrors>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public void Put_WhenWalidationError_ReturnsServiceErrorsResult()
+        {
+            var mockUserService = A.Fake<IUserService>();
+            A.CallTo(() => mockUserService.GetByName(A<string>._)).Returns(new User());
+
+            A.CallTo(() => mockUserService.Update(A<UpdateUserData>._, A<IValidationErrors>._)).Invokes(
+                    (UpdateUserData model, IValidationErrors errors) => errors.Add("", "error"));
+
+            var controller = CreateController(mockUserService);
+
+
+            var result = controller.Put("", new UpdateUserData());
+
+
+            Assert.IsInstanceOf<ServiceErrorsResult>(result);
+        }
+
+        private UsersController CreateController(IUserService userService = null)
+        {
+            userService = userService ?? A.Fake<IUserService>();
+
+            return new UsersController()
+            {
+                UserService = userService
+            };
+        }
+    }
+}
